cdff5f1eb3b4c88d64fb28904d3d75878639bba7
Artis3n <dev@artis3nal.com>
2020-01-11 01:26:24 -0500

    Project structure set up, graphql query created

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    clearMocks: true,
+    moduleFileExtensions: ['js'],
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.js'],
+    testRunner: 'jest-circus/runner',
+    verbose: true,
+};
